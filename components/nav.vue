017de8f3b73e7be7c987cf7c3f661ae9a6efcc44
Krsiak Daniel <krsiak.daniel@gmail.com>
2023-10-09 17:56:32 +0200

    add icons in nav

--- components/nav.vue
+++ components/nav.vue
@@ -7,6 +7,20 @@
     app
     :style="{padding : $vuetify.breakpoint.mdAndUp ? '0px 100px' : ''}"
   >
+  <v-toolbar-title @click="$router.push('/')" class="text-md-5 font-weight-bold pointer">
+    eCommerce site
+  </v-toolbar-title>
+  <v-spacer/>
+  <v-btn icon>
+    <v-icon size="20">mdi-store-outline</v-icon>
+  </v-btn>
+  <v-btn icon>
+    <v-icon size="20">mdi-cart-outline</v-icon>
+  </v-btn>
+  <v-divider vertical class="mx-md-5 mx-2" />
+  <v-btn icon>
+    <v-icon size="20">mdi-brightness-7</v-icon>
+  </v-btn>
   </v-app-bar>
  </div>
 </template>
