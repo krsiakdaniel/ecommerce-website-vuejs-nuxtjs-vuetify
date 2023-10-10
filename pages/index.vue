2ec85bad91a56ba87b9c60a0b505f0bc16b20b0f
Krsiak Daniel <krsiak.daniel@gmail.com>
2023-10-10 10:57:03 +0200

    feature - carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
     <Nav />
+    <HomeCarousel :sale_items="sale_items" />
     <Footer />
   </div>
 </template>
 
 <script>
-export default {
+import HomeCarousel from '../components/HomeCarousel.vue';
 
+export default {
+    async created() {
+        this.sale_items = await this.$content('products').where({ onSale: true }).fetch();
+        this.products = await this.$content('products').fetch();
+    },
+    data() {
+        return {
+            products: null,
+            sale_items: null,
+        };
+    },
+    components: { HomeCarousel }
 }
 </script>
 
